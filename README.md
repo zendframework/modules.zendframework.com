31e9c0656e7339f0f19ebe16d8b5feed234079d2
Evan Coury <me@evancoury.com>
2011-10-25 18:25:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+ZFModules.com
+=============
+Version 0.0.1
+
+Introduction
+------------
+ZFModules.com will be a community site for publishing and sharing Zend Framework
+modules.
+
+Deployment
+----------
+The production branch of this repository is automatically deployed live to
+zfmodules.com every 10 minutes.
---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// Ensure ZF is on the include path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(__DIR__ . '/../library'),
+    realpath(__DIR__ . '/../library/ZendFramework/library'),
+    get_include_path(),
+)));
+
+require_once 'Zend/Loader/AutoloaderFactory.php';
+Zend\Loader\AutoloaderFactory::factory(array('Zend\Loader\StandardAutoloader' => array()));
+
+$appConfig = new Zend\Config\Config(include __DIR__ . '/../configs/application.config.php');
+
+$moduleLoader = new Zend\Loader\ModuleAutoloader($appConfig['module_paths']);
+$moduleLoader->register();
+
+$moduleManager = new Zend\Module\Manager(
+    $appConfig['modules'],
+    new Zend\Module\ManagerOptions($appConfig['module_manager_options'])
+);
+
+// Create application, bootstrap, and run
+$bootstrap      = new Zend\Mvc\Bootstrap($moduleManager);
+$application    = new Zend\Mvc\Application;
+$bootstrap->bootstrap($application);
+$application->run()->send();
