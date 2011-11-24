8560b80ad52b006c78a83b9938e9cf7c7fcf8bbf
Evan Coury <me@evancoury.com>
2011-11-23 17:07:52 -0700

    Swittching back to use skeleton dir names to avoid confusing anyone

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 require_once dirname(__DIR__) . '/vendor/ZendFramework/library/Zend/Loader/AutoloaderFactory.php';
 Zend\Loader\AutoloaderFactory::factory(array('Zend\Loader\StandardAutoloader' => array()));
 
-$appConfig = include dirname(__DIR__) . '/etc/application.config.php';
+$appConfig = include dirname(__DIR__) . '/config/application.config.php';
 
 $moduleLoader = new Zend\Loader\ModuleAutoloader($appConfig['module_paths']);
 $moduleLoader->register();
@@ -10,7 +10,7 @@ $moduleLoader->register();
 $moduleManager = new Zend\Module\Manager($appConfig['modules']);
 $listenerOptions = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
 $moduleManager->setDefaultListenerOptions($listenerOptions);
-$moduleManager->getConfigListener()->addConfigGlobPath(dirname(__DIR__) . '/etc/conf.d/*.config.php');
+$moduleManager->getConfigListener()->addConfigGlobPath(dirname(__DIR__) . '/config/autoload/*.config.php');
 $moduleManager->loadModules();
 
 // Create application, bootstrap, and run
