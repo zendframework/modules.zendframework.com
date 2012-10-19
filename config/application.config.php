133282dc7efa08483a8eec82216d00f613215c8d
Hounddog <martin.shwalbe@gmail.com>
2012-10-19 21:30:03 +0200

    Some testing

--- config/application.config.php
+++ config/application.config.php
@@ -11,6 +11,7 @@ return array(
         'AssetManager',
         'Assetic',
         'HybridAuth',
+        'EdpGithub'
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
