5f6d194db0d22d2aadf07518e45e24f2b2a24d8c
PEM <pe.manteau@gmail.com>
2012-10-22 21:34:36 +0200

    Added some Exception Handling in Application Controller

---
+++ module/Application/src/Application/Controller/Exception/InvalidArgumentException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Application\Controller\Exception;
+
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
---
+++ module/Application/src/Application/Controller/Exception/UnexpectedValueException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Application\Controller\Exception;
+
+class UnexpectedValueException extends \UnexpectedValueException implements ExceptionInterface
+{
+}
