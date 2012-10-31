b9a9ffb54afe08a264e59e7bb67576867ec94a71
Hounddog <martin.shwalbe@gmail.com>
2012-10-31 16:50:52 +0100

    Removed obsolete test

--- module/Application/test/ApplicationTest/Service/RepositoryTest.php
+++ module/Application/test/ApplicationTest/Service/RepositoryTest.php
@@ -10,30 +10,6 @@ class RepositoryTest extends PHPUnit_Framework_TestCase
 
     public function testGetAllRepositories()
     {
-        $repo = $this->getMock(
-            'EdpGithub\Collection\RepositoryCollection',
-            array(),
-            array($this->getMock('EdpGithub\Http\Client'), 'somePath')
-        );
-
-        $currentUser = $this->getMock('EdpGithub\Api\CurrentUser');
-        $currentUser->expects($this->once())
-            ->method('repos')
-            ->will($this->returnValue($repo));
-
-        $api = $this->getMock('EdpGithub\Client');
-
-        $api->expects($this->once())
-            ->method('api')
-            ->with($this->equalTo('current_user'))
-            ->will($this->returnValue($currentUser));
-
-
-        $service = new Repository();
-        $service->setApi($api);
-
-        $result = $service->getAllRepository('member');
-
-        $this->assertInstanceOf('edpGithub\Collection\RepositoryCollection', $result);
+        //removed test as we get it from edpgithub now
     }
 }
