82b51096be00d9d4e5ed6886786ea8cdc06c1797
Hounddog <martin.shwalbe@gmail.com>
2012-11-08 12:15:28 +0100

    Added Class map and Views to template_map

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -29,6 +29,9 @@ class Module
     public function getAutoloaderConfig()
     {
         return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -1,6 +1,14 @@
 <?php
 return array(
     'view_manager' => array(
+        'template_map' => array(
+            'helper/module'                 =>  __DIR__ . '/../view/helper/module.phtml',
+            'scn-social-auth/user/login'    =>  __DIR__ . '/../view/scn-social-auth/user/login.phtml',
+            'user/helper/new-users'         =>  __DIR__ . '/../view/user/helper/new-users.phtml',
+            'user/module/orgs'              =>  __DIR__ . '/../view/user/module/orgs.phtml',
+            'user/module/repos'             =>  __DIR__ . '/../view/user/module/repos.phtml',
+            'zfc-user/user/index'           =>  __DIR__ . '/../view/zfc-user/user/index.phtml',
+        ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
