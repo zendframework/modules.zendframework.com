a95223b143f6f2e05b9205114c7ced3beed197fe
Martin <martin.shwalbe@gmail.com>
2014-07-24 18:37:01 +0200

    Fixed integration error for Constant APPLICATION_PATH

--- public/index.php
+++ public/index.php
@@ -7,6 +7,10 @@ chdir(dirname(__DIR__));
 // Setup autoloading
 include 'init_autoloader.php';
 
+if (!defined('APPLICATION_PATH')) {
+    define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
+}
+
 $appConfig = include APPLICATION_PATH . '/config/application.config.php';
 
 if (file_exists(APPLICATION_PATH . '/config/development.config.php')) {
