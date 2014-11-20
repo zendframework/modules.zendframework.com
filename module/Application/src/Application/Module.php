e5cdb81eb799aa12acf5412d121f31649ae2b0f1
Andreas Möller <am@localheinz.com>
2014-11-20 16:32:16 +0100

    Enhancement: Do not implement AutoloaderProviderInterface where not needed

--- module/Application/src/Application/Module.php
+++ module/Application/src/Application/Module.php
@@ -25,18 +25,4 @@ class Module
     {
         return include __DIR__ . '/config/module.config.php';
     }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
 }
--- module/ZfModule/src/ZfModule/Module.php
+++ module/ZfModule/src/ZfModule/Module.php
@@ -9,27 +9,11 @@
 
 namespace ZfModule;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Cache\StorageFactory;
 
-class Module implements AutoloaderProviderInterface
+class Module
 {
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-            // if we're in a namespace deeper than one level we need to fix the \ in the path
-                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/', __NAMESPACE__),
-                ),
-            ),
-        );
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
