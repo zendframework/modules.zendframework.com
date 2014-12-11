cc71b1723f036cdbe4b8b3c60c99750bdd0481e1
Andreas Möller <am@localheinz.com>
2014-12-11 17:15:17 +0100

    Fix: Remove autoloader initialization script

--- module/Application/test/bootstrap.php
+++ module/Application/test/bootstrap.php
@@ -3,4 +3,6 @@
 error_reporting(E_ALL | E_STRICT);
 chdir(dirname(__DIR__) . '/../..');
 
+require_once 'vendor/autoload.php';
+
 ApplicationTest\Integration\Util\Bootstrap::setConfig(include 'config/application.config.php');
