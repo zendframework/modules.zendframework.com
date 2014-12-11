1a182a8f4f328320669988aa57da02d9c4518369
Andreas Möller <am@localheinz.com>
2014-12-11 17:13:43 +0100

    Fix: Move bootstrap script into test directory

--- module/Application/test/bootstrap.php
+++ module/Application/test/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
 error_reporting(E_ALL | E_STRICT);
-chdir(__DIR__);
+chdir(dirname(__DIR__) . '/../..');
 
 ApplicationTest\Integration\Util\Bootstrap::setConfig(include 'config/application.config.php');
