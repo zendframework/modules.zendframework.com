d7db19efcee9e6db9028c74f07294c7b7929203a
Andreas Möller <am@localheinz.com>
2015-01-18 17:23:41 +0100

    Enhancement: Leverage AbstractHttpControllerTestCase

--- module/Application/test/ApplicationTest/Integration/Controller/IndexControllerTest.php
+++ module/Application/test/ApplicationTest/Integration/Controller/IndexControllerTest.php
@@ -2,60 +2,25 @@
 
 namespace ApplicationTest\Integration\Controller;
 
-use Application\Controller;
 use ApplicationTest\Integration\Util\Bootstrap;
 use Zend\Http;
-use Zend\Mvc\Controller\ControllerManager;
-use Zend\Mvc\MvcEvent;
-use Zend\Mvc\Router\RouteMatch;
-use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
-use PHPUnit_Framework_TestCase;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
-class IndexControllerTest extends PHPUnit_Framework_TestCase
+class IndexControllerTest extends AbstractHttpControllerTestCase
 {
-    /**
-     * @var Controller\IndexController;
-     */
-    protected $controller;
-    protected $request;
-    protected $response;
-
-    /**
-     * @var RouteMatch
-     */
-    protected $routeMatch;
-
-    protected $event;
-
     protected function setUp()
     {
-        $serviceManager = Bootstrap::getServiceManager();
+        parent::setUp();
 
-        /* @var ControllerManager $controllerManager */
-        $controllerManager = $serviceManager->get('ControllerManager');
-        $this->controller = $controllerManager->get('Application\Controller\Index');
-
-        $this->request = new Http\Request();
-        $this->routeMatch = new RouteMatch(['controller' => 'index']);
-        $this->event = new MvcEvent();
-        $config = $serviceManager->get('Config');
-        $routerConfig = isset($config['router']) ? $config['router'] : [];
-        $router = HttpRouter::factory($routerConfig);
-        $this->event->setRouter($router);
-        $this->event->setRouteMatch($this->routeMatch);
-        $this->controller->setEvent($this->event);
-        $this->controller->setServiceLocator($serviceManager);
+        $this->setApplicationConfig(Bootstrap::getConfig());
     }
 
     public function testIndexActionCanBeAccessed()
     {
-        $this->routeMatch->setParam('action', 'index');
+        $this->dispatch('/');
 
-        $this->controller->dispatch($this->request);
-
-        /* @var Response $response */
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(Http\Response::STATUS_CODE_200, $response->getStatusCode());
+        $this->assertControllerName('Application\Controller\Index');
+        $this->assertActionName('index');
+        $this->assertResponseStatusCode(Http\Response::STATUS_CODE_200);
     }
 }
