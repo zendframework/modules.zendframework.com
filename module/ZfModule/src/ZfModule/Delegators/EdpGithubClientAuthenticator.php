21b498ab2216ad5ad137f9f3e94fdf6f3491564f
Gary Hockin <gary@hock.in>
2015-01-22 15:40:08 +0100

    Fixed petty complaints on comments

--- module/ZfModule/src/ZfModule/Delegators/EdpGithubClientAuthenticator.php
+++ module/ZfModule/src/ZfModule/Delegators/EdpGithubClientAuthenticator.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Created by Gary Hockin.
- * Date: 22/01/2015
- * @GeeH
- */
 
 namespace ZfModule\Delegators;
 
@@ -15,18 +10,13 @@ class EdpGithubClientAuthenticator implements DelegatorFactoryInterface
 {
 
     /**
-     * A factory that creates delegates of a given service
-     *
-     * @param ServiceLocatorInterface $serviceLocator the service locator which requested the service
-     * @param string $name the normalized service name
-     * @param string $requestedName the requested service name
-     * @param callable $callback the callback that is responsible for creating the service
+     * {@inheritDoc}
      *
      * @return Client
      */
     public function createDelegatorWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName, $callback)
     {
-        /** @var Client $client */
+        /* @var Client $client */
         $client = $callback();
         $config = $serviceLocator->get('config')['scn-social-auth'];
         $client->authenticate('UrlClientId', $config['github_client_id'], $config['github_secret']);
