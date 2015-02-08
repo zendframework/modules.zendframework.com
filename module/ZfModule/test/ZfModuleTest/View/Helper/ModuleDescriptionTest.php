2ef9b42679f203ffba88505e392dcf637ed5521a
Andreas Möller <am@localheinz.com>
2015-02-08 11:29:03 +0100

    Fix: Pass in repository instead of array

--- module/ZfModule/test/ZfModuleTest/View/Helper/ModuleDescriptionTest.php
+++ module/ZfModule/test/ZfModuleTest/View/Helper/ModuleDescriptionTest.php
@@ -3,6 +3,7 @@
 namespace ZfModuleTest\View\Helper;
 
 use PHPUnit_Framework_TestCase;
+use stdClass;
 use Zend\View;
 use ZfModule\View\Helper;
 
@@ -10,9 +11,14 @@ class ModuleDescriptionTest extends PHPUnit_Framework_TestCase
 {
     public function testInvokeRendersViewScript()
     {
-        $module = [
-            'foo' => 'bar',
-        ];
+        $module = new stdClass();
+        $module->owner = new stdClass();
+        $module->owner->login = 'foo';
+        $module->owner->avatar_url = 'http://www.example.org/john.gif';
+        $module->name = 'bar';
+        $module->created_at = '1970-01-01 00:00:00';
+        $module->html_url = 'http://www.example.org';
+        $module->description = 'blah blah blah';
 
         $view = $this->getMockForAbstractClass(View\Renderer\RendererInterface::class);
 
@@ -22,7 +28,12 @@ class ModuleDescriptionTest extends PHPUnit_Framework_TestCase
             ->with(
                 $this->equalTo('zf-module/helper/module-description.phtml'),
                 $this->equalTo([
-                    'module' => $module,
+                    'owner' => $module->owner->login,
+                    'name' => $module->name,
+                    'createdAt' => $module->created_at,
+                    'url' => $module->html_url,
+                    'photoUrl' => $module->owner->avatar_url,
+                    'description' => $module->description,
                 ])
             )
         ;
