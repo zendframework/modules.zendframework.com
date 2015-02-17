a6e5049fca8b05fb6d332f95e8239b2d7af378c7
Andreas Möller <am@localheinz.com>
2015-02-17 17:03:08 +0100

    Enhancement: Allow ModuleView to process stdClass and entity

--- module/ZfModule/src/ZfModule/View/Helper/ModuleView.php
+++ module/ZfModule/src/ZfModule/View/Helper/ModuleView.php
@@ -2,20 +2,61 @@
 
 namespace ZfModule\View\Helper;
 
+use InvalidArgumentException;
+use stdClass;
 use Zend\View\Helper\AbstractHelper;
+use ZfModule\Entity;
 
 class ModuleView extends AbstractHelper
 {
     /**
-     * @param array $module
+     * @param Entity\Module|stdClass $module
      * @param string $button
      * @return string
      */
     public function __invoke($module, $button = 'submit')
     {
-        return $this->getView()->render('zf-module/helper/module-view.phtml', [
-            'module' => $module,
+        $values = $this->fetchValues($module);
+
+        $values += [
             'button' => $button,
-        ]);
+        ];
+
+        return $this->getView()->render('zf-module/helper/module-view.phtml', $values);
+    }
+
+    /**
+     * @param stdClass|Entity\Module$module
+     * @return array
+     */
+    private function fetchValues($module)
+    {
+        if ((!$module instanceof stdClass) && !($module instanceof Entity\Module)) {
+            throw new InvalidArgumentException(sprintf(
+                'Parameter "%s" needs to be specified as %s',
+                '$module',
+                'an instance of stdClass or ZfModule\Entity\Module'
+            ));
+        }
+
+        if ($module instanceof stdClass) {
+            return [
+                'owner' => $module->owner->login,
+                'name' => $module->name,
+                'createdAt' => $module->created_at,
+                'url' => $module->html_url,
+                'photoUrl' => $module->owner->avatar_url,
+                'description' => $module->description,
+            ];
+        }
+
+        return [
+            'owner' => $module->getOwner(),
+            'name' => $module->getName(),
+            'createdAt' => $module->getCreatedAt(),
+            'url' => $module->getUrl(),
+            'photoUrl' => $module->getPhotoUrl(),
+            'description' => $module->getDescription(),
+        ];
     }
 }
--- module/ZfModule/test/ZfModuleTest/View/Helper/ModuleViewTest.php
+++ module/ZfModule/test/ZfModuleTest/View/Helper/ModuleViewTest.php
@@ -3,16 +3,16 @@
 namespace ZfModuleTest\View\Helper;
 
 use PHPUnit_Framework_TestCase;
+use stdClass;
 use Zend\View;
+use ZfModule\Entity;
 use ZfModule\View\Helper;
 
 class ModuleViewTest extends PHPUnit_Framework_TestCase
 {
-    public function testInvokeRendersViewScript()
+    public function testInvokeHandlesRepository()
     {
-        $module = [
-            'foo' => 'bar',
-        ];
+        $repository = $this->repository();
 
         $view = $this->getMockForAbstractClass(View\Renderer\RendererInterface::class);
 
@@ -22,7 +22,41 @@ class ModuleViewTest extends PHPUnit_Framework_TestCase
             ->with(
                 $this->equalTo('zf-module/helper/module-view.phtml'),
                 $this->equalTo([
-                    'module' => $module,
+                    'owner' => $repository->owner->login,
+                    'name' => $repository->name,
+                    'createdAt' => $repository->created_at,
+                    'url' => $repository->html_url,
+                    'photoUrl' => $repository->owner->avatar_url,
+                    'description' => $repository->description,
+                    'button' => 'submit',
+                ])
+            )
+        ;
+
+        $helper = new Helper\ModuleView();
+        $helper->setView($view);
+
+        $helper($repository);
+    }
+
+    public function testInvokeHandlesModule()
+    {
+        $module = $this->module();
+
+        $view = $this->getMockForAbstractClass(View\Renderer\RendererInterface::class);
+
+        $view
+            ->expects($this->once())
+            ->method('render')
+            ->with(
+                $this->equalTo('zf-module/helper/module-view.phtml'),
+                $this->equalTo([
+                    'owner' => $module->getOwner(),
+                    'name' => $module->getName(),
+                    'createdAt' => $module->getCreatedAt(),
+                    'url' => $module->getUrl(),
+                    'photoUrl' => $module->getPhotoUrl(),
+                    'description' => $module->getDescription(),
                     'button' => 'submit',
                 ])
             )
@@ -34,12 +68,24 @@ class ModuleViewTest extends PHPUnit_Framework_TestCase
         $helper($module);
     }
 
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvokeDoesNotHandleAnythingElse()
+    {
+        $module = 'foo';
+
+        $view = $this->getMockForAbstractClass(View\Renderer\RendererInterface::class);
+
+        $helper = new Helper\ModuleView();
+        $helper->setView($view);
+
+        $helper($module);
+    }
+
     public function testInvokeAllowsSpecifyingButtonType()
     {
-        $module = [
-            'foo' => 'bar',
-        ];
-
+        $module = $this->module();
         $button = 'baz';
 
         $view = $this->getMockForAbstractClass(View\Renderer\RendererInterface::class);
@@ -49,10 +95,12 @@ class ModuleViewTest extends PHPUnit_Framework_TestCase
             ->method('render')
             ->with(
                 $this->equalTo('zf-module/helper/module-view.phtml'),
-                $this->equalTo([
-                    'module' => $module,
-                    'button' => $button,
-                ])
+                $this->logicalAnd(
+                    $this->arrayHasKey('button'),
+                    $this->callback(function ($values) use ($button) {
+                        return $values['button'] === $button;
+                    })
+                )
             )
         ;
 
@@ -60,5 +108,41 @@ class ModuleViewTest extends PHPUnit_Framework_TestCase
         $helper->setView($view);
 
         $helper($module, $button);
+    }
+
+    /**
+     * @return stdClass
+     */
+    private function repository()
+    {
+        $repository = new stdClass();
+
+        $repository->name = 'foo';
+        $repository->description = 'blah blah';
+        $repository->created_at = '1970-01-01 00:00:00';
+        $repository->html_url = 'http://www.example.org';
+
+        $repository->owner = new stdClass();
+        $repository->owner->login = 'suzie';
+        $repository->owner->avatar_url = 'http://www.example.org/img/suzie.gif';
+
+        return $repository;
+    }
+
+    /**
+     * @return Entity\Module
+     */
+    private function module()
+    {
+        $module = new Entity\Module();
+
+        $module->setName('foo');
+        $module->setDescription('blah blah');
+        $module->setCreatedAt('1970-01-01 00:00:00');
+        $module->setUrl('http://www.example.org');
+        $module->setOwner('suzie');
+        $module->setPhotoUrl('http://www.example.org/img/suzie.gif');
+
+        return $module;
     }
 }
