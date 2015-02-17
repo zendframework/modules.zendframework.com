46115ca265fd2c07d5a02e236e1df448974ab768
Andreas Möller <am@localheinz.com>
2015-02-17 19:44:59 +0100

    Enhancement: Simplify view helper as there's only one use case

--- module/ZfModule/src/ZfModule/View/Helper/ModuleView.php
+++ module/ZfModule/src/ZfModule/View/Helper/ModuleView.php
@@ -2,10 +2,8 @@
 
 namespace ZfModule\View\Helper;
 
-use InvalidArgumentException;
 use stdClass;
 use Zend\View\Helper\AbstractHelper;
-use ZfModule\Entity;
 
 class ModuleView extends AbstractHelper
 {
@@ -13,53 +11,20 @@ class ModuleView extends AbstractHelper
     const BUTTON_REMOVE = 'remove';
 
     /**
-     * @param Entity\Module|stdClass $module
+     * @param stdClass $module
      * @param string $button
      * @return string
      */
     public function __invoke($module, $button = 'submit')
     {
-        $values = $this->fetchValues($module);
-
-        $values += [
+        return $this->getView()->render('zf-module/helper/module-view.phtml', [
+            'owner' => $module->owner->login,
+            'name' => $module->name,
+            'createdAt' => $module->created_at,
+            'url' => $module->html_url,
+            'photoUrl' => $module->owner->avatar_url,
+            'description' => $module->description,
             'button' => $button,
-        ];
-
-        return $this->getView()->render('zf-module/helper/module-view.phtml', $values);
-    }
-
-    /**
-     * @param stdClass|Entity\Module$module
-     * @return array
-     */
-    private function fetchValues($module)
-    {
-        if ((!$module instanceof stdClass) && !($module instanceof Entity\Module)) {
-            throw new InvalidArgumentException(sprintf(
-                'Parameter "%s" needs to be specified as %s',
-                '$module',
-                'an instance of stdClass or ZfModule\Entity\Module'
-            ));
-        }
-
-        if ($module instanceof stdClass) {
-            return [
-                'owner' => $module->owner->login,
-                'name' => $module->name,
-                'createdAt' => $module->created_at,
-                'url' => $module->html_url,
-                'photoUrl' => $module->owner->avatar_url,
-                'description' => $module->description,
-            ];
-        }
-
-        return [
-            'owner' => $module->getOwner(),
-            'name' => $module->getName(),
-            'createdAt' => $module->getCreatedAt(),
-            'url' => $module->getUrl(),
-            'photoUrl' => $module->getPhotoUrl(),
-            'description' => $module->getDescription(),
-        ];
+        ]);
     }
 }
--- module/ZfModule/test/ZfModuleTest/View/Helper/ModuleViewTest.php
+++ module/ZfModule/test/ZfModuleTest/View/Helper/ModuleViewTest.php
@@ -5,7 +5,6 @@ namespace ZfModuleTest\View\Helper;
 use PHPUnit_Framework_TestCase;
 use stdClass;
 use Zend\View;
-use ZfModule\Entity;
 use ZfModule\View\Helper;
 
 class ModuleViewTest extends PHPUnit_Framework_TestCase
@@ -39,53 +38,9 @@ class ModuleViewTest extends PHPUnit_Framework_TestCase
         $helper($repository);
     }
 
-    public function testInvokeHandlesModule()
-    {
-        $module = $this->module();
-
-        $view = $this->getMockForAbstractClass(View\Renderer\RendererInterface::class);
-
-        $view
-            ->expects($this->once())
-            ->method('render')
-            ->with(
-                $this->equalTo('zf-module/helper/module-view.phtml'),
-                $this->equalTo([
-                    'owner' => $module->getOwner(),
-                    'name' => $module->getName(),
-                    'createdAt' => $module->getCreatedAt(),
-                    'url' => $module->getUrl(),
-                    'photoUrl' => $module->getPhotoUrl(),
-                    'description' => $module->getDescription(),
-                    'button' => 'submit',
-                ])
-            )
-        ;
-
-        $helper = new Helper\ModuleView();
-        $helper->setView($view);
-
-        $helper($module);
-    }
-
-    /**
-     * @expectedException \InvalidArgumentException
-     */
-    public function testInvokeDoesNotHandleAnythingElse()
-    {
-        $module = 'foo';
-
-        $view = $this->getMockForAbstractClass(View\Renderer\RendererInterface::class);
-
-        $helper = new Helper\ModuleView();
-        $helper->setView($view);
-
-        $helper($module);
-    }
-
     public function testInvokeAllowsSpecifyingButtonType()
     {
-        $module = $this->module();
+        $repository = $this->repository();
         $button = 'baz';
 
         $view = $this->getMockForAbstractClass(View\Renderer\RendererInterface::class);
@@ -107,7 +62,7 @@ class ModuleViewTest extends PHPUnit_Framework_TestCase
         $helper = new Helper\ModuleView();
         $helper->setView($view);
 
-        $helper($module, $button);
+        $helper($repository, $button);
     }
 
     /**
@@ -127,22 +82,5 @@ class ModuleViewTest extends PHPUnit_Framework_TestCase
         $repository->owner->avatar_url = 'http://www.example.org/img/suzie.gif';
 
         return $repository;
-    }
-
-    /**
-     * @return Entity\Module
-     */
-    private function module()
-    {
-        $module = new Entity\Module();
-
-        $module->setName('foo');
-        $module->setDescription('blah blah');
-        $module->setCreatedAt('1970-01-01 00:00:00');
-        $module->setUrl('http://www.example.org');
-        $module->setOwner('suzie');
-        $module->setPhotoUrl('http://www.example.org/img/suzie.gif');
-
-        return $module;
     }
 }
