9cca91470c280d1f5ad121275c46975754b999aa
Andreas Möller <am@localheinz.com>
2015-02-18 14:20:13 +0100

    Enhancement: Assert pagination() finds in name and description

--- module/ZfModule/test/ZfModuleTest/Integration/Mapper/ModuleTest.php
+++ module/ZfModule/test/ZfModuleTest/Integration/Mapper/ModuleTest.php
@@ -4,6 +4,8 @@ namespace ZfModuleTest\Integration\Mapper;
 
 use ApplicationTest\Integration\Util\Bootstrap;
 use PHPUnit_Framework_TestCase;
+use Zend\Db;
+use ZfModule\Entity;
 use ZfModule\Mapper;
 
 /**
@@ -11,13 +13,92 @@ use ZfModule\Mapper;
  */
 class ModuleTest extends PHPUnit_Framework_TestCase
 {
-    public function testCanRetrieveService()
+    /**
+     * @var Mapper\Module
+     */
+    private $mapper;
+
+    /**
+     * @var Db\Adapter\Driver\ConnectionInterface
+     */
+    private $connection;
+
+    protected function setUp()
     {
         $serviceManager = Bootstrap::getServiceManager();
 
-        $this->assertInstanceOf(
-            Mapper\Module::class,
-            $serviceManager->get(Mapper\Module::class)
-        );
+        $this->mapper = $serviceManager->get(Mapper\Module::class);
+
+        /* @var Db\Adapter\Adapter $database */
+        $database = $serviceManager->get('zfcuser_zend_db_adapter');
+
+        $this->connection = $database->getDriver()->getConnection();
+        $this->connection->beginTransaction();
+    }
+
+    protected function tearDown()
+    {
+        $this->connection->rollback();
+
+        unset($this->mapper);
+        unset($this->connection);
+    }
+
+    /**
+     * @dataProvider providerProperties
+     *
+     * @param $property
+     */
+    public function testPaginationFindsInProperty($property)
+    {
+        $value = 'foo bar baz';
+        $query = 'bar';
+
+        $module = $this->module();
+
+        $setter = 'set' . ucfirst($property);
+
+        $module->{$setter}($value);
+
+        $this->mapper->insert($module);
+
+        $paginator = $this->mapper->pagination(1, 100, $query);
+
+        $this->assertSame(1, $paginator->getTotalItemCount());
+    }
+
+    /**
+     * @return array
+     */
+    public function providerProperties()
+    {
+        return [
+            [
+                'name',
+            ],
+            [
+                'description',
+            ],
+        ];
+    }
+
+    /**
+     * @return Entity\Module
+     */
+    private function module()
+    {
+        static $id = 1;
+
+        $module = new Entity\Module();
+
+        $module->setId($id);
+        $module->setName('');
+        $module->setOwner('');
+        $module->setDescription('');
+        $module->setUrl('');
+
+        $id++;
+
+        return $module;
     }
 }
