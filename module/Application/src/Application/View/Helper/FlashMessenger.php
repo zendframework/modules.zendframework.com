474612ebb8c3ff1533670479eea5fb48e4953257
Marco Rieger <marco.rieger@aubi-plus.de>
2015-02-24 16:03:46 +0100

    added different logic

--- module/Application/src/Application/View/Helper/FlashMessenger.php
+++ module/Application/src/Application/View/Helper/FlashMessenger.php
@@ -6,46 +6,61 @@ use Zend\View\Helper\FlashMessenger as ZendFlashMessenger;
 
 class FlashMessenger extends ZendFlashMessenger
 {
+    /** @var array */
+    private $classOptions = [];
+
     /**
      * Render Messages
      *
      * @param  string $namespace
-     * @param  array  $classes
+     * @param  array $classes
      * @return string
      */
     public function render($namespace = PluginFlashMessenger::NAMESPACE_DEFAULT, array $classes = array())
     {
-        // set classes
-        $this->classMessages = array(
-            'error' 	=> 'alert alert-dismissable alert-danger',
-            'default' 	=> 'alert alert-dismissable alert-warning',
-            'info' 		=> 'alert alert-dismissable alert-info',
-            'success'	=> 'alert alert-dismissable alert-success',
-        );
+        $this->classOptions = [
+            PluginFlashMessenger::NAMESPACE_INFO => [
+                'name' => $this->getTranslator()->translate('Information'),
+                'class' => 'alert alert-info'
+            ],
+            PluginFlashMessenger::NAMESPACE_ERROR => [
+                'name' => $this->getTranslator()->translate('Error'),
+                'class' => 'alert alert-danger'
+            ],
+            PluginFlashMessenger::NAMESPACE_SUCCESS => [
+                'name' => $this->getTranslator()->translate('Success'),
+                'class' => 'alert alert-success'
+            ],
+            PluginFlashMessenger::NAMESPACE_DEFAULT => [
+                'name' => $this->getTranslator()->translate('Message'),
+                'class' => 'alert alert-info'
+            ],
+            PluginFlashMessenger::NAMESPACE_WARNING => [
+                'name' => $this->getTranslator()->translate('Warning'),
+                'class' => 'alert alert-warning'
+            ],
+        ];
 
-        $this->setMessageSeparatorString('</li><li>');
-        $this->setMessageCloseString('</li></ul></div>');
+        // if custom namespace handle as info message
+        if (!isset($this->classOptions[$namespace])) {
+            $this->classOptions[$namespace] = $this->classOptions[PluginFlashMessenger::NAMESPACE_INFO];
+        }
 
-        $this->setMessageOpenFormat(
-            sprintf('<div%%s><h4>%s</h4><ul><li>', $this->getTranslator()->translate('Warning', $this->getTranslatorTextDomain()))
-        );
-        $default = parent::render(PluginFlashMessenger::NAMESPACE_DEFAULT);
+        $messageOutput = '';
 
-        $this->setMessageOpenFormat(
-            sprintf('<div%%s><h4>%s</h4><ul><li>', $this->getTranslator()->translate('Error', $this->getTranslatorTextDomain()))
-        );
-        $error = parent::render(PluginFlashMessenger::NAMESPACE_ERROR);
+        foreach ($this->classMessages as $currentNamespace => $css) {
+            $namespaceOptions = $this->classOptions[$currentNamespace];
 
-        $this->setMessageOpenFormat(
-            sprintf('<div%%s><h4>%s</h4><ul><li>', $this->getTranslator()->translate('Information', $this->getTranslatorTextDomain()))
-        );
-        $info = parent::render(PluginFlashMessenger::NAMESPACE_INFO);
+            $this->classMessages[$currentNamespace] = $namespaceOptions['class'];
+            $openingString = sprintf('<div%%s><span class="sr-only">%s</span>', $namespaceOptions['name']);
 
-        $this->setMessageOpenFormat(
-            sprintf('<div%%s><h4>%s</h4><ul><li>', $this->getTranslator()->translate('Success', $this->getTranslatorTextDomain()))
-        );
-        $success = parent::render(PluginFlashMessenger::NAMESPACE_SUCCESS);
+            $this->setMessageOpenFormat($openingString);
+            $this->setMessageSeparatorString(sprintf('</div>%s', $openingString));
+            $this->setMessageCloseString('</div>');
 
-        return $success . $error . $default . $info;
+            $messageOutput .= parent::render($currentNamespace, $classes);
+        }
+
+        return $messageOutput;
     }
 }
