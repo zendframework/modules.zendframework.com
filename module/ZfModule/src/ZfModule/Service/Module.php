0e1bff2b2c8dca022693787297f25faafb4e4296
Andreas Möller <am@localheinz.com>
2015-02-24 11:24:01 +0100

    Fix: Loosely compare

--- module/ZfModule/src/ZfModule/Service/Module.php
+++ module/ZfModule/src/ZfModule/Service/Module.php
@@ -131,7 +131,7 @@ class Module extends EventProvider
                 return false;
             }
 
-            if (null === $this->moduleMapper->findByUrl($repository->html_url)) {
+            if (!$this->moduleMapper->findByUrl($repository->html_url)) {
                 return false;
             }
 
