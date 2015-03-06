c3284a89f166d0f15bf8bb50e6b66756e102b159
Marco Rieger <marco.rieger@aubi-plus.de>
2015-03-06 13:24:14 +0100

    classes now final - added  return self

--- module/ZfModule/src/ZfModule/Controller/Exception/InvalidDataException.php
+++ module/ZfModule/src/ZfModule/Controller/Exception/InvalidDataException.php
@@ -5,18 +5,18 @@ namespace ZfModule\Controller\Exception;
 use Application\Exception;
 use Zend\Http;
 
-class InvalidDataException extends Exception\ViewableUserException
+final class InvalidDataException extends Exception\ViewableUserException
 {
     /**
      * Generates an Exception from a Invalid Post Request
      *
      * @param string $publicMessage
      * @param Http\Request $request
-     * @return static
+     * @return self
      */
-    final public static function fromInvalidRequest($publicMessage, Http\Request $request)
+    public static function fromInvalidRequest($publicMessage, Http\Request $request)
     {
-        return new static(
+        return new self(
             sprintf('Invalid Request received [%s]', $request->toString()),
             (string) $publicMessage
         );
--- module/ZfModule/src/ZfModule/Controller/Exception/RepositoryException.php
+++ module/ZfModule/src/ZfModule/Controller/Exception/RepositoryException.php
@@ -5,7 +5,7 @@ namespace ZfModule\Controller\Exception;
 use Application\Exception;
 use Zend\Http;
 
-class RepositoryException extends Exception\ViewableUserException
+final class RepositoryException extends Exception\ViewableUserException
 {
     /**
      * Generates an Exception when an invalid Repository is requested
@@ -13,11 +13,11 @@ class RepositoryException extends Exception\ViewableUserException
      * @param string $publicMessage
      * @param string $repositoryOwner
      * @param string $repositoryName
-     * @return static
+     * @return self
      */
-    final public static function fromNotFoundRepository($publicMessage, $repositoryOwner, $repositoryName)
+    public static function fromNotFoundRepository($publicMessage, $repositoryOwner, $repositoryName)
     {
-        return new static(
+        return new self(
             sprintf('Invalid Repository requested [%s/%s]', $repositoryOwner, $repositoryName),
             (string) $publicMessage,
             Http\Response::STATUS_CODE_404
@@ -29,11 +29,11 @@ class RepositoryException extends Exception\ViewableUserException
      *
      * @param string $publicMessage
      * @param string $repositoryUrl
-     * @return static
+     * @return self
      */
-    final public static function fromNotFoundRepositoryUrl($publicMessage, $repositoryUrl)
+    public static function fromNotFoundRepositoryUrl($publicMessage, $repositoryUrl)
     {
-        return new static(
+        return new self(
             sprintf('Invalid Repository from URL requested [%s]', $repositoryUrl),
             (string) $publicMessage,
             Http\Response::STATUS_CODE_404
@@ -46,11 +46,11 @@ class RepositoryException extends Exception\ViewableUserException
      * @param string $publicMessage
      * @param string $repositoryIdentifier
      * @param string[] $requiredPermissions
-     * @return static
+     * @return self
      */
-    final public static function fromInsufficientPermissions($publicMessage, $repositoryIdentifier, array $requiredPermissions)
+    public static function fromInsufficientPermissions($publicMessage, $repositoryIdentifier, array $requiredPermissions)
     {
-        return new static(
+        return new self(
             sprintf('Invalid Repository permission [%s] required [%s]', $repositoryIdentifier, implode(',', $requiredPermissions)),
             (string) $publicMessage,
             Http\Response::STATUS_CODE_403
@@ -62,11 +62,11 @@ class RepositoryException extends Exception\ViewableUserException
      *
      * @param string $publicMessage
      * @param string $repositoryIdentifier
-     * @return static
+     * @return self
      */
-    final public static function fromNonModuleRepository($publicMessage, $repositoryIdentifier)
+    public static function fromNonModuleRepository($publicMessage, $repositoryIdentifier)
     {
-        return new static(
+        return new self(
             sprintf('Invalid Repository - No ZF Module [%s]', $repositoryIdentifier),
             (string) $publicMessage,
             Http\Response::STATUS_CODE_403
