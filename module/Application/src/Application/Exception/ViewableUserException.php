d8199a591187aec281882730b1c23b105c031ff8
Marco Rieger <marco.rieger@aubi-plus.de>
2015-03-06 13:23:49 +0100

    added interface + make private var

--- module/Application/src/Application/Exception/ViewableUserException.php
+++ module/Application/src/Application/Exception/ViewableUserException.php
@@ -2,17 +2,24 @@
 
 namespace Application\Exception;
 
-class ViewableUserException extends \Exception
+class ViewableUserException extends \Exception implements ViewableUserExceptionInterface
 {
-    protected $publicMessage;
+    /* @var string */
+    private $publicMessage;
 
+    /**
+     * @inheritdoc
+     */
     public function __construct($message = '', $publicMessage = '', $code = 0, \Exception $previous = null)
     {
         $this->publicMessage = (string) $publicMessage;
         parent::__construct($message, $code, $previous);
     }
 
-    final public function getPublicMessage()
+    /**
+     * @inheritdoc
+     */
+    public function getPublicMessage()
     {
         return $this->publicMessage;
     }
